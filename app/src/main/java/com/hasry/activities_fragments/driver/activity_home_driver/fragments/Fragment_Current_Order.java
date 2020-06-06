b322696de9e9a4df2cc6d2a35d121072ce384c75
End Point <ahmedmohamed23345@gmail.com>
2020-06-06 13:14:58 +0200

    ch

--- app/src/main/java/com/hasry/activities_fragments/driver/activity_home_driver/fragments/Fragment_Current_Order.java
+++ app/src/main/java/com/hasry/activities_fragments/driver/activity_home_driver/fragments/Fragment_Current_Order.java
@@ -158,6 +158,12 @@ public class Fragment_Current_Order extends Fragment implements GoogleApiClient.
 
                                 }
                             } else {
+                                try {
+
+                                    Log.e("error", response.code() + "_" + response.errorBody().string());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
                                 if (response.code() == 500) {
                                     Toast.makeText(activity, "Server Error", Toast.LENGTH_SHORT).show();
 
